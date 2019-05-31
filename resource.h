3c76c518331f313604cf01bfc9f897172c089d83
Joe Schroedl <joe.schroedl@outlook.com>
2019-05-31 15:01:31 -0400

    Added keyboard shortcuts to about box.

---
+++ resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MandelbrotCpp.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MANDELBROTCPP_DIALOG        102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MANDELBROTCPP               107
+#define IDI_SMALL                       108
+#define IDC_MANDELBROTCPP               109
+#define IDR_MAINFRAME                   128
+#define ID_FILE_RESETZOOM               32771
+#define ID_ALGORITHM_MANDELBROT         32772
+#define ID_ALGORITHM_JULIA              32773
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
