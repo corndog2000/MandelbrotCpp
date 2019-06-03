96d152eb8c27c6888c30b25b7903c6bab4d08a57
Joe Schroedl <joe.schroedl@outlook.com>
2019-06-03 09:58:29 -0400

    Maxiterations added to resource.h

--- resource.h
+++ resource.h
@@ -15,6 +15,14 @@
 #define ID_FILE_RESETZOOM               32771
 #define ID_ALGORITHM_MANDELBROT         32772
 #define ID_ALGORITHM_JULIA              32773
+#define ID_FILE_SAVEIMAGE               32774
+#define ID_MAXITERATIONS_255            32775
+#define ID_MAXITERATIONS_1000           32776
+#define ID_MAXITERATIONS_5000           32777
+#define ID_MAXITERATIONS_10000          32778
+#define ID_MAXITERATIONS_1000000        32779
+#define ID_MAXITERATIONS_25             32780
+#define ID_MAXITERATIONS_50             32781
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -23,8 +31,8 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_COMMAND_VALUE         32782
 #define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           110
+#define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
